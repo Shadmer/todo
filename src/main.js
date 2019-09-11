80d10c2e2d5b2e11ddd2f664e79dfe33cfb51c50
Shadmer <shadmer89@gmail.com>
2019-09-11 21:01:14 +0700

    init project

--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
+const debug = process.env.NODE_ENV !== 'production';
+if (debug) {
+    axios.defaults.baseURL = 'http://todo/';
+}
+
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
